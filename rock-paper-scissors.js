0b151c49be42e9e4cead47be88326bfea037764f
Simon Zaslavsky <szaslavsky6@gmail.com>
2024-07-12 11:34:05 -0500

    Created function to get user input.

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,11 +1,32 @@
+
+
 // Create function that randomly returns "rock", "paper" or "scissors"
 function getComputerChoise() {
     const choices = ["rock", "paper", "scissors"];
     const randomNumber = Math.floor(Math.random() * 3);
     return choices[randomNumber];
 }
-console.log(getComputerChoise());
+//console.log(getComputerChoise());
 // Create function that takes user input and returns it
+function getHumanChoice() {
+    let human;
+    let pick = prompt("Enter your choice! (rock, paper or scissors)").toLowerCase();
+    if(pick === "rock"){
+        human = 0;
+    }
+    else if(pick === "paper"){
+        human = 1;
+    }
+    else if(pick === "scissors"){
+        human = 2;
+    }
+    else{
+        console.log("Pick from the choices provided");
+        return getHumanChoice(); // Ask user for new input if invalid choice
+    }
+    return human;
+}
+console.log(getHumanChoice());
 // Keep track of players score
     // create humanScore and computerScore variables
 // Write logic to play a single round
